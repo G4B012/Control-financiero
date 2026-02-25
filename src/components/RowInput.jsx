dd60668378307b6c62efeb99db745b75ffb96832
G4B012 <aquinojosegabriel124@gmail.com>
2026-02-25 16:04:07 -0400

    Enhance RowInput styling for right alignment

--- src/components/RowInput.jsx
+++ src/components/RowInput.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function RowInput({ value, onChange, placeholder, type="text", right=false, small=false }){
   return (
     <input
-      className={(small ? "inputSm" : "input") + (right ? " text-right" : "")}
+     className={
+        (small ? "inputSm" : "input") +
+        (right ? " text-right font-semibold tracking-wide" : "")
+      }
       value={value}
       type={type}
       placeholder={placeholder}
